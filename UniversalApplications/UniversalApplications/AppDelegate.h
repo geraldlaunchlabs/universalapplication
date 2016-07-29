402b0d8c31c834a48afa6f277be764efb8da7a22
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-07-29 11:49:50 +0800

    project initialization

---
+++ UniversalApplications/UniversalApplications/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UniversalApplications
+//
+//  Created by LLDM on 29/07/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UniversalApplications/UniversalApplications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UniversalApplications
+//
+//  Created by LLDM on 29/07/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
